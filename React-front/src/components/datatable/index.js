95babb6d3288b148fa9855467097bdb5dd3aa9ed
Mateus <limamunhozmateus@gmail.com>
2025-10-13 16:22:28 -0300

    groselha na tela home

--- React-front/src/components/datatable/index.js
+++ React-front/src/components/datatable/index.js
@@ -1,13 +1,44 @@
 import { Container, Title, Button } from './style';
-
+import { useContext } from 'react';
+import { useNavigate } from 'react-router';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import DropTitle from "../droptitle";
+import UserContext from '../../contexts/UserContext'
+import { Client, removeToken } from '../../api/client';
+import { removePermissions } from '../../service/PermissionService'
+import { getDataUser, removeDataUser } from '../../service/UserService'
 export default function DataTable1() {
+  const { user } = useContext(UserContext)
+  const dataUser = getDataUser()
+  const navigate = useNavigate();
+
+  function logout() {
+
+    setTimeout(() => {
+      Client.post('auth/logout').then(res => {
+        removeToken()
+        removePermissions()
+        removeDataUser()
+        navigate('/login')
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+        .finally(() => {
+        })
+
+    }, 1000)
+  }
 
   return (
     <Container>
       <Title>BANIF bank</Title>
       <>
         <Button>Transferência</Button>
-        <Button>Sair</Button>
+        <NavDropdown title={<DropTitle text={dataUser ? dataUser.fullName : 'Visitante'} />} id="navbarScrollingDropdown" className="me-4">
+          <NavDropdown.Item href="#" className="me-5">{dataUser ? dataUser.email : 'visitante@gmail.com'}</NavDropdown.Item>
+          <NavDropdown.Item onClick={() => logout()} className="me-5">Sair</NavDropdown.Item>
+        </NavDropdown>
       </>
     </Container>
   );
